913fa714ab8ffcaba2f93a74febe9a8cfdb485b5
John McMahon <idahosound@gmail.com>
2021-11-20 11:09:00 -0700

    display random heading on button click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import headings from "./headings";
 
 function App() {
+
+const [headingText, setHeadingText] = useState("Hello")
+
+function randomHeading(){
+  const i = Math.floor(Math.random() * headings.length);
+  const randomText = headings[i];
+  return randomText
+}
+
+function btnClick(){
+  setHeadingText(randomHeading);
+}
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick = {btnClick}>Submit</button>
     </div>
   );
 }
---
+++ src/components/headings.js
@@ -0,0 +1,15 @@
+const headings = [
+    "Hello",
+    "Psych!",
+    "Bet you thought that would work",
+    "Click harder",
+    "Try again",
+    "Nope",
+    "If only it were that easy",
+    "Made you click",
+    "ZZZZZ",
+    "Gone fishing",
+    "This one goes to 11"
+]
+
+export default headings;
